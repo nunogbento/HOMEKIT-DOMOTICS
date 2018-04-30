bde0d462cfd2abed8f1bec4eb3380f5f6d2d7761
Nuno Bento <nuno.bento@marlo.pt>
2018-04-30 09:56:53 +0100

    PCCWD API with Initialization, power loss resilience. Mordomus Lights moved to mqtt plugin

--- pccwdapi/app.js
+++ pccwdapi/app.js
@@ -5,6 +5,7 @@ var mqtt = require('mqtt')
 
 var Service, Characteristic;
 var serialPort;
+
 const ReadLine=SerialPort.parsers.Readline;
 
 var serialPort = new SerialPort('/dev/ttyUSB0', {
@@ -12,8 +13,9 @@ var serialPort = new SerialPort('/dev/ttyUSB0', {
     stopBits: 1
 });
 
-const client = mqtt.connect('mqtt://127.0.0.1')
+const mqttClient = mqtt.connect('mqtt://127.0.0.1')
 
+var self=this;
 
 var status={};
 
@@ -23,7 +25,17 @@ serialPort.on('error', function(err) {
 	
 const parser = serialPort.pipe(new ReadLine({ delimiter: [0x46] }));
 
-function setStatus(key){
+self.SendQueue = [];
+
+
+
+const SerialWriteQueue = setInterval(() => {  //BECAUSE PCWDD is realy slow...
+	if(self.SendQueue.length > 0){		
+		serialPort.write([self.SendQueue.shift()]);		
+	}
+  }, 100);
+
+function setStatus(key){	
 	switch (key.Id)
 	{
 		case 75:
@@ -55,32 +67,55 @@ function setStatus(key){
 	}	
 }  
 
-parser.on('data', function(data){	
-	
-	if(data.charCodeAt(0)==0x49 && data.charCodeAt(1)==65533) {
-		var key={
-			Id:data.charCodeAt(2),
-			Count:data.charCodeAt(3)
-		};
-		setStatus(key);
-		try {
-			client.publish('pccwd/keypress', JSON.stringify(key) )
-		}catch(err) {
-			 console.log('Error: ', err.message);  
+const initPccwd=[
+0xaa,0x02,0x8a,0xaa,0x02,0x8b,0xaa,0x02,0x8a,0xaa,0x02,0x8c,0xaa,0x02,0x8a,0xaa,0x02,0x8d,
+0xaa,0x02,0x8a,0xaa,0x02,0x8e,0xaa,0x02,0x8a,0xaa,0x02,0x8f,0xaa,0x02,0x8a,0xaa,0x02,0x90,
+0xaa,0x02,0x8a,0xaa,0x02,0x91,0xaa,0x02,0x8a,0xaa,0x02,0x92,0xaa,0x02,0xc9,0xaa,0x02,0xc9
+];
+    
+parser.on('data', function(data){		
+	if(data.charCodeAt(0)==0x49 && data.charCodeAt(1)==65533) {		
+		if(data.charCodeAt(2)==0x01 && data.charCodeAt(3)==0x67){
+			console.log('Connected to PCCwd'); 
+			clearInterval(pingInterval);
+			self.SendQueue=self.SendQueue.concat(initPccwd);	
+			console.log('Pccwd Initialized');
+		}else{
+			var key={
+				Id:data.charCodeAt(2),
+				Count:data.charCodeAt(3)
+			};
+			setStatus(key);
+			try {
+				mqttClient.publish('pccwd/keypress', JSON.stringify(key) )
+			}catch(err) {
+				 console.log('Error: ', err.message);  
+			}
 		}
 	}else{
 		console.log('->'+ data);  		
 	}
 });
 
+mqttClient.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  
+  client.end()
+})
+
+
+const pingInterval = setInterval(() => {
+	self.SendQueue=self.SendQueue.concat([0x57,0x01,0x64]);
+    console.log('.');
+}, 800);
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var routes = require("./routes/routes.js")(app,serialPort,status);
-
+var routes = require("./routes/routes.js")(app,this,status);
 
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
--- pccwdapi/routes/routes.js
+++ pccwdapi/routes/routes.js
@@ -1,21 +1,15 @@
-var appRouter = function(app,serialPort,status) {
+var appRouter = function(app,application,status) {
   var self=this;
-  self.serialPort=serialPort;
+ 
   self.status=status;
   
   app.get("/turnon", function(req, res) {
     if(!req.query.address) {
         return res.send({"status": "error", "message": "missing address"});
     }
-    var addr = parseInt(req.query.address);
-
-    
-        self.serialPort.write([0xAA]);
-        self.serialPort.write([addr]);
-        self.serialPort.write([0x64]);
-		self.status[req.query.address]=true;
-    
-        
+    var addr = parseInt(req.query.address);    
+	application.SendQueue=application.SendQueue.concat([0xaa,addr,0x64]);		
+	self.status[req.query.address]=true;
     res.sendStatus(200);
   });
 
@@ -23,23 +17,17 @@ var appRouter = function(app,serialPort,status) {
     if(!req.query.address) {
         return res.send({"status": "error", "message": "missing address"});
     }
-    var addr = parseInt(req.query.address);
-    
-        self.serialPort.write([0xAA]);
-        self.serialPort.write([addr]);
-        self.serialPort.write([0x01]);
-		self.status[req.query.address]=false;
-    
+    var addr = parseInt(req.query.address);    
+	application.SendQueue=application.SendQueue.concat([0xaa,addr,0x01]);	
+	self.status[req.query.address]=false;    
     res.sendStatus(200);
-    });
-	
+  });
+		
   app.get("/status", function(req, res) {
     if(!req.query.address) {
         return res.send({"status": "error", "message": "missing address"});
     }
     res.send((self.status.hasOwnProperty(req.query.address) && self.status[req.query.address])? "1":"0");
-    });
-
+  });
 }
-
 module.exports = appRouter;
